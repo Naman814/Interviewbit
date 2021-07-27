c84f01b183abe824a12bec6992bab149c95ec72a
Naman vijay <namanvijay814@gmail.com>
2021-07-28 01:37:57 +0530

    array

---
+++ Arrays/Next_permutation.cpp
@@ -0,0 +1,34 @@
+
+vector<int> Solution::nextPermutation(vector<int> &A) {
+         
+        int index1=-1,index2=-1;
+        int n=A.size();
+        if(n<=1)
+        {
+        return A;
+        }
+    for(int i=n-2;i>=0;i--)
+    {
+        if(A[i]<A[i+1])
+        {
+            index1=i;
+            break;
+        }
+    }
+        if(index1>=0)
+        {
+    for(int i=n-1;i>=0;i--)
+    {
+        if(A[i]>A[index1])
+        {
+            index2=i;
+            break;
+        }
+    }   
+    swap(A[index1],A[index2]);
+    reverse(A.begin()+index1+1,A.end());
+    return A;
+        }
+    reverse(A.begin(),A.end());
+    return A;
+}
