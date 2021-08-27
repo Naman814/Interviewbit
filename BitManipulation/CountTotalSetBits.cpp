ac518818678347ba147c1ebb8f106776f62a4dff
Naman vijay <namanvijay814@gmail.com>
2021-08-28 03:33:12 +0530

    updated

--- BitManipulation/CountTotalSetBits.cpp
+++ BitManipulation/CountTotalSetBits.cpp
@@ -1,5 +1,25 @@
 
-// TC : O(n^2)
+// Approach 1 : Recursive solution
+
+int Solution::solve(int A)
+{
+    if (A == 0)
+    {
+        return 0;
+    }
+    long long x = 1;
+    while ((1 << x) <= A)
+    {
+        x++;
+    }
+    x = x - 1;
+    long long before = x * (1 << (x - 1));
+    long long curr = A - (1 << x) + 1;
+    long long remain = A - (1 << x);
+    return (before + curr + solve(remain)) % 1000000007;
+}
+
+// Approach 2 : TC : O(n^2)
 int Solution::solve(int A)
 {
     int count = 0;
@@ -15,4 +35,3 @@ int Solution::solve(int A)
     }
     return count;
 }
-
